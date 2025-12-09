5cd6322fe5053cef503440cd1ce7eaa2082a0240
venkatpandi <144114794+Venkateshwaranpandiyan@users.noreply.github.com>
2025-12-09 15:32:07 +0530

    Add files via upload

---
+++ WorkerDemo/WorkerDemo/src/main/java/com/example/WorkerDemo/Model/Employee.java
@@ -0,0 +1,10 @@
+package com.example.WorkerDemo.Model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Employee {
+    private Long id;
+    private String name;
+    private String department;
+}
